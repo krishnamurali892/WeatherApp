bdd8ab91ff778805c6ee487f855ebe438fafb34d
Murali Krishna Reddy <krishnamurali892@gmail.com>
2021-06-22 09:52:57 +0530

    Update Constants.kt

--- app/src/main/java/com/murali/weatherapp/utils/Constants.kt
+++ app/src/main/java/com/murali/weatherapp/utils/Constants.kt
@@ -5,6 +5,6 @@ class Constants {
     companion object{
         const val APP_ID = "59b9c49620ceb0f2da6159019d7af0d3"
         const val BASE_URL = "https://api.openweathermap.org/"
-        const val LOCATION_UPDATE_INTERVAL:Long = 3*60//3 min for 2 hr 2*60*60
+        const val LOCATION_UPDATE_INTERVAL:Long = 5*1000//5 seconds for 2 hr 1000*(2*60*60)
     }
 }
